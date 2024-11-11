39526de58b3357151909a77e8f220df969da3839
sd <sd@fathallaiats.edu>
2024-11-11 09:33:50 +0200

    library project

---
+++ Library v2/Library v2/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Library_v2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        libraryEntities1 _context =new libraryEntities1();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+      
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string x = "Admin";
+            string y = "123";
+
+
+
+
+             if (name.Text == x && passo.Text == y)
+            {
+              
+
+                Window5 window5 = new Window5();
+                window5.Show();
+                this.Close();
+                
+               
+
+            }
+            
+
+            string username = name.Text;
+            string password = passo.Text;
+
+            
+            var Ju = _context.Members.FirstOrDefault(u => u.user_name == username && u.password.ToString() == password);
+
+               if (Ju != null)
+            {
+
+                Window4 window4 = new Window4();
+                window4.Show();
+                this.Close();
+
+                 
+            }
+            else
+            {
+               
+                MessageBox.Show("Invalid username or password. Please try again.", "Login Failed", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window2 window2 = new Window2();
+            window2.Show();
+            this.Close();
+        }
+
+      
+    }
+}
---
+++ Library v2/Library v2/Window4.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Library_v2
+{
+    /// <summary>
+    /// Interaction logic for Window4.xaml
+    /// </summary>
+    public partial class Window4 : Window
+    {
+        books_catalog _selctedbook = new books_catalog();
+        libraryEntities1 _context = new libraryEntities1();
+        public Window4()
+        {
+            InitializeComponent();
+            Load();
+        }
+        private void Load()
+        {
+            list.ItemsSource = _context.books_catalog.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            MessageBox.Show("downloaded book succesfully");
+
+        }
+
+        private void list_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            _selctedbook = (books_catalog)list.SelectedItem;
+            bookname.Text = _selctedbook.book_title;
+        }
+    }
+}
